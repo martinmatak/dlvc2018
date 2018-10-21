ddc4b83dfbb46972f12811c98278017ee3d3a448
Martin Matak <martin.matak@gmail.com>
2018-10-21 17:50:05 +0200

    Implements Batch Generator

--- dlvc/batches.py
+++ dlvc/batches.py
@@ -1,7 +1,12 @@
 
 from .dataset import Dataset
-
+import numpy as np
 import types
+
+IMAGE_WIDTH = 32
+IMAGE_HEIGHT = 32
+CHANNEL_NUM = 3
+
 
 class Batch:
     '''
@@ -14,8 +19,9 @@ class Batch:
         '''
 
         self.data = None
-        self.label = None
+        self.labels = None
         self.idx = None
+
 
 class BatchGenerator:
     '''
@@ -37,25 +43,66 @@ class BatchGenerator:
         Raises ValueError on invalid argument values, such as if num is > len(dataset).
         '''
 
-        # TODO implement
+        # dataset field
+        if isinstance(dataset, Dataset):
+            self.dataset = dataset
+        else:
+            raise TypeError("dataset has an inappropriate type.")
 
-        pass
+        # num field
+        if isinstance(num, int):
+            if num > len(dataset):
+                raise ValueError("Number may not be grater than length of a dataset.")
+            else:
+                self.batch_size = num
+        else:
+            raise TypeError("num has an inappropriate type.")
+
+        # shuffle field
+        if isinstance(shuffle, bool):
+            self.shuffle = shuffle
+        else:
+            raise TypeError("shuffle has an inappropriate type.")
+
+        # op field
+        if op is None:
+            self.op = op
+        elif isinstance(op, types.FunctionType):
+            self.op = op
+        else:
+            raise TypeError("op has an inappropriate type.")
+
+        # initialize indices
+        if shuffle:
+            self.indices = np.random.permutation(len(dataset))
+        else:
+            self.indices = np.arange(len(dataset))
 
     def __len__(self) -> int:
         '''
         Returns the number of batches generated per iteration.
         '''
 
-        # TODO implement
-
-        pass
+        return int(np.floor(len(self.dataset) / self.batch_size))
 
     def __iter__(self) -> types.GeneratorType:
         '''
         Iterate over the wrapped dataset, returning the data as batches.
         '''
 
-        # TODO implement
-        # The "yield" keyword makes this easier
+        data = np.zeros((self.batch_size, IMAGE_WIDTH, IMAGE_HEIGHT, CHANNEL_NUM), dtype=np.uint8)
+        labels = np.zeros((self.batch_size, 1), dtype=np.int32)
+        indices = np.zeros(self.batch_size, dtype=np.int32)
+        for idx in  range(0, len(self)):
+            sample_indices = self.indices[idx * self.batch_size:(idx + 1) * self.batch_size]
+            for i, sample_id in enumerate(sample_indices):
+                _, sample = self.dataset.__getitem__(sample_id)
+                data[i] = sample['data']
+                labels[i] = sample['label']
+                indices[i] = sample['idx']
+            batch = Batch()
+            batch.data = data
+            batch.labels = labels
+            batch.idx = indices
+            yield batch
 
-        pass
