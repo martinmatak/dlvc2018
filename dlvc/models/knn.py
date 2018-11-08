Optimize L1 computation, fix accuracy comparison bug

dlvc/models/knn.py
@@ -135,16 +135,18 @@ class KnnClassifier(Model):
     def L1distance(self, vectorA: np.ndarray, vectorB: np.ndarray):
         if len(vectorA) != len(vectorB):
             raise ValueError("Vectors have different dimensions.")
-        return sum(abs(x1 - x2) for x1, x2 in zip(vectorA, vectorB))
+        return np.abs(vectorA - vectorB).sum()
 
     def getNeighbors(self, instance):
         '''
         Return k nearest neighbors.
         '''
         distances = []
+
         for x in range(len(self.data)):
             dist = self.L1distance(instance, self.data[x])
             distances.append((self.labels[x], self.data[x], dist))
+
         distances.sort(key=operator.itemgetter(2))
         neighbors = []
         for x in range(self.k):

dlvc/tests/endToEndTests.py
@@ -4,15 +4,11 @@ from dlvc.models.knn import KnnClassifier
 import numpy as np
 from dlvc.batches import BatchGenerator
 from dlvc.ops import vectorize, chain, type_cast
+import time
 
 '''
 Tests in this file should test the whole pipeline. They take more time than unit tests.
 '''
-
-# Important: This test takes A LOT of time, in order to speed it up, redefine distance computation method in knn.py
-# (i.e. use some third-party implementation / use L2 ..)
-# e.g. https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cityblock.html#scipy.spatial.distance.cityblock
-
 
 # make sure the whole pipeline works:
 #  when k=1 and
@@ -20,6 +16,7 @@ Tests in this file should test the whole pipeline. They take more time than unit
 #  batch size is equal to whole dataset then
 #  kNN must have accuracy 100%
 
+start = time.time()
 
 pets = PetsDataset('/Users/mmatak/dev/college/DLVC/cifar-10/cifar-10-batches-py/', Subset.TEST)
 num_classes = 2
@@ -49,7 +46,9 @@ for batch in batchGenerator:
     accuracy = measure_accuracy(predictedLabels, groundTruthLabels)
 assert abs(accuracy - 1.0) < treshold, "Accuracy is different than 1!"
 
+end = time.time()
 ####################################################
 print("If this line gets executed, all end-to-end tests passed successfully :)")
+print("total time needed: " + str(end-start) + " seconds")
 #####################################################
 

knn_cats_dogs.py
@@ -25,10 +25,12 @@ batchGenerator_validation = BatchGenerator(pets_validation, len(pets_validation)
 batchGenerator_test = BatchGenerator(pets_test, len(pets_test), False,
                                          op=chain([type_cast(dtype=np.float32), vectorize()]))
 
-best_accuracy = 0.0
+best_accuracy = Accuracy()
 best_k = -1
 results = {}
-for k in range(1, 101, 7):  # grid search example
+knn = None
+
+for k in range(1, 100, 40):  # grid search example
     knn = KnnClassifier(k, IMAGE_HEIGHT*IMAGE_WIDTH*NUM_CHANNELS, NUM_CLASSES)
     accuracy = Accuracy()
 
@@ -52,7 +54,6 @@ for key, value in results.items():
     print("k=" + str(key), ", accuracy=", str(value))
 
 # compute test accuracy
-knn = KnnClassifier(best_k, IMAGE_HEIGHT * IMAGE_WIDTH * NUM_CHANNELS, NUM_CLASSES)
 test_batch = None
 test_batch_predictions = None
 accuracy = Accuracy()