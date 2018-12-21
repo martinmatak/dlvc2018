cfbc15e8b23920c2188b46f898a80c64c082c29d
khaftool <44157927+khaftool@users.noreply.github.com>
2018-12-21 19:02:09 +0100

    Added Mul() add() chw2hwc() hwc2chw()

--- assignment2/dlvc/ops.py
+++ assignment2/dlvc/ops.py
@@ -5,6 +5,7 @@ from typing import List, Callable
 # See https://docs.python.org/3/library/typing.html#typing.Callable for what this line means
 Op = Callable[[np.ndarray], np.ndarray]
 
+
 def chain(ops: List[Op]) -> Op:
     '''
     Chain a list of operations together.
@@ -41,27 +42,59 @@ def vectorize() -> Op:
 
     return op
 
+
 def hwc2chw() -> Op:
     '''
-    Flip a 3D array with shape HWC to shape CHW.
+    Flip a 3D array with shape HWC(0,1,2) to shape CHW(2,0,1).
     '''
-     # TODO implement (see np.transpose)
-     pass
- def chw2hwc() -> Op:
+
+    def op(sample: np.ndarray) -> np.ndarray:
+        sample = sample.transpose(2, 0, 1)
+        return sample
+
+    return op
+
+
+def chw2hwc() -> Op:
     '''
-    Flip a 3D array with shape CHW to HWC.
+    Flip a 3D array with shape CHW(0,1,2) to HWC(1,2,0).
     '''
-     # TODO implement (see np.transpose)
-     pass
- def add(val: float) -> Op:
+
+    def op(sample: np.ndarray) -> np.ndarray:
+        sample = sample.transpose(1, 2, 0)
+        return sample
+
+    return op
+
+
+def add(val: float) -> Op:
     '''
     Add a scalar value to all array elements.
     '''
-     # TODO implement
-     pass
- def mul(val: float) -> Op:
+
+    def op(sample: np.ndarray) -> np.ndarray:
+        sample += val
+        return sample
+
+    return op
+
+
+def mul(val: float) -> Op:
     '''
     Multiply all array elements by the given scalar.
     '''
-     # TODO implement
 
+    def op(sample: np.ndarray) -> np.ndarray:
+        sample = np.multiply(sample, val)
+        return sample
+
+    return op
+
+def norm() -> Op:
+    '''
+    Per-channel normalization based on statistics of training set
+    '''
+
+    #TODO implement
+
+    pass
