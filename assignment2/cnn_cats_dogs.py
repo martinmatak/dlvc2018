d639d274d459740fbaa7763852484a0f1644499a
khaftool <44157927+khaftool@users.noreply.github.com>
2018-12-23 19:47:03 +0100

    Fixed input CNN-dimension

--- assignment2/cnn_cats_dogs.py
+++ assignment2/cnn_cats_dogs.py
@@ -25,17 +25,19 @@ pets_validation = PetsDataset(dir, Subset.VALIDATION)
 pets_test = PetsDataset(dir, Subset.TEST)
 
 batchGenerator_training = BatchGenerator(pets_training, BATCH_SIZE, False,
-                                         op=chain([type_cast(dtype=np.float32), add(-127.5), mul(1 / 127.5), hwc2chw()]))
+                                         op=chain(
+                                             [type_cast(dtype=np.float32), add(-127.5), mul(1 / 127.5), hwc2chw()]))
 batchGenerator_validation = BatchGenerator(pets_validation, BATCH_SIZE, False,
-                                           op=chain([type_cast(dtype=np.float32), add(-127.5), mul(1 / 127.5)]))
+                                           op=chain(
+                                               [type_cast(dtype=np.float32), add(-127.5), mul(1 / 127.5), hwc2chw()]))
 batchGenerator_test = BatchGenerator(pets_test, BATCH_SIZE, False,
-                                     op=chain([type_cast(dtype=np.float32), add(-127.5), mul(1 / 127.5)]))
+                                     op=chain([type_cast(dtype=np.float32), add(-127.5), mul(1 / 127.5), hwc2chw()]))
 
 class CatDogNet(nn.Module):
     def __init__(self):
         super(CatDogNet, self).__init__()
-        # First Layer 2xConv and Max pool out_Shape = (15x15x32)
-        self.conv1_layer1 = nn.Conv2d(in_channels=NUM_CHANNELS, out_channels=32, kernel_size=3, stride=1, padding=1)
+        # First Layer 2xConv and Max pool out_Shape = (16x16x32)
+        self.conv1_layer1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.relu1_layer1 = nn.ReLU()
 
         self.conv2_layer1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1)
@@ -43,29 +45,38 @@ class CatDogNet(nn.Module):
 
         self.max_pool_layer1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        # second Layer 3xConv and Max pool out_shape = (6x6x64)
+        # second Layer 2xConv and Max pool out_shape = (8x8x64)
         self.conv1_layer2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.relu1_layer2 = nn.ReLU()
 
         self.conv2_layer2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.relu2_layer2 = nn.ReLU()
 
-        self.conv3_layer2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.relu3_layer2 = nn.ReLU()
+        self.max_pool_layer2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.avg_pool_layer2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        # Third Layer 2xConv and average pool out_shape = (4x4x128)
+        self.conv1_layer3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.relu1_layer3 = nn.ReLU()
+
+        self.conv2_layer3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.relu2_layer3 = nn.ReLU()
+
+        self.avg_pool_layer3 = nn.AvgPool2d(kernel_size=2, stride=2)
 
         # Add all the units into the Sequential layer in exact order
-        self.net = nn.Sequential(self.conv1_layer1, self.relu1_layer1, self.conv2_layer1, self.relu2_layer1,
-                                 self.max_pool_layer1, self.conv1_layer2, self.relu1_layer2, self.conv2_layer2,
-                                 self.relu2_layer2, self.conv3_layer2, self.relu3_layer2, self.avg_pool_layer2)
+        self.cnn_net = nn.Sequential(self.conv1_layer1, self.relu1_layer1, self.conv2_layer1, self.relu2_layer1,
+                                 self.max_pool_layer1,
+                                 self.conv1_layer2, self.relu1_layer2, self.conv2_layer2, self.relu2_layer2,
+                                 self.max_pool_layer2,
+                                 self.conv1_layer3, self.relu1_layer3, self.conv2_layer3, self.relu2_layer3,
+                                 self.avg_pool_layer3)
 
-        self.fc = nn.Linear(in_features=2304, out_features=NUM_CLASSES)
+        self.fc = nn.Linear(in_features=2048, out_features=NUM_CLASSES)
 
     # override
     def forward(self, input):
-        output = self.net(input)
-        output = output.view(-1, 6 * 6 * 64)
+        output = self.cnn_net(input)
+        output = output.view(-1, 4 * 4 * 128)
         output = self.fc(output)
         return output
 
@@ -73,7 +84,7 @@ net = CatDogNet()
 clf = CnnClassifier(net, (BATCH_SIZE, NUM_CHANNELS, IMAGE_HEIGHT, IMAGE_WIDTH), NUM_CLASSES, lr, wd)
 loss_list = []
 for epoch in range(0, EPOCHS):
-    print("Epoche: ", epoch+1)
+    print("Epoche: ", epoch + 1)
 
     for batch in batchGenerator_training:
         loss = clf.train(batch.data, batch.labels)
@@ -85,7 +96,7 @@ for epoch in range(0, EPOCHS):
 
     accuracy = Accuracy()
     for batch in batchGenerator_validation:
-        predictions = net.predict(batch.data)
-        accuracy.update(predictions, batch.labels)
+        predictions = clf.predict(batch.data)
+        accuracy.update(predictions.detach().numpy(), batch.labels)
 
-    print("Val" + str(accuracy))
+    print("Val " + str(accuracy))
