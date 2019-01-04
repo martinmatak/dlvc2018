fix accuracy calculation

assignment2/dlvc/tests/endToEndTests.py
@@ -13,7 +13,6 @@ Tests in this file should test the whole pipeline. They take more time than unit
 # make sure the whole pipeline works:
 #  when k=1 and
 #  training and predict subset are equal and
-#  batch size is equal to whole dataset then
 #  kNN must have accuracy 100%
 
 start = time.time()
@@ -22,7 +21,7 @@ pets = PetsDataset('/Users/mmatak/dev/college/DLVC/cifar-10/cifar-10-batches-py/
 num_classes = 2
 k = 1
 knn = KnnClassifier(k, 32*32*3, num_classes)
-batchGenerator = BatchGenerator(pets, len(pets), False, op=chain([type_cast(dtype=np.float32), vectorize()]))
+batchGenerator = BatchGenerator(pets, 512, False, op=chain([type_cast(dtype=np.float32), vectorize()]))
 
 groundTruthLabels = None
 for batch in batchGenerator: