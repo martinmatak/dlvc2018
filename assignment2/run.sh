208014aec5f0795fa0bcbe36558447af4c14bfa1
khaftool <khaftool>
2019-01-11 20:54:19 +0100

    modified

--- assignment2/run.sh
+++ assignment2/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -S /bin/bash
 #PBS -m bea
-#PBS -M martin.matak@gmail.com
+#PBS -M aliamini.r@gmail.com
 
 # Request free GPU
 export CUDA_VISIBLE_DEVICES=$(getFreeGPU)
-python3 /home/e1635889/group15/submissions/assignment1/martin/dlvc2018/assignment2/cnn_cats_dogs_pt3.py
+python3 /home/e1227507/group15/submissions/assignment1/AliA/dlvc2018/assignment2/cnn_cats_dogs.py
